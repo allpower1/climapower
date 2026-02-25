37d7e99116f93311f86c8012ce86d8bc080e41d1
Moises Cuevas <mcuevasp@desarrollosocial.cl>
2026-02-25 11:18:41 -0300

    mejorar diseño sitios de errores en caso de fallar algo

--- core_climapower/resources/views/errors/504.blade.php
+++ core_climapower/resources/views/errors/504.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.appsitioweb')
 
 @section('content')
-<div class="site-breadcrumb" style="padding-top:20px;padding-bottom:20px;">
-    <div class="container" style="text-align: left;">
-        <ul class="breadcrumb-menu">
-            <li class="lihidden"><a href="{{ url('/') }}">Home</a></li>
-            <li class="active lihidden">Error 504</li>
-        </ul>
-    </div>
-</div>
-<div class="error-area py-120">
-    <div class="container">
-        <div class="col-md-8 mx-auto">
-            <div class="error-wrapper">
-                <h2>Oops...</h2>
-                <p>Lo sentimos, pero algo salió mal.</p>
-                <a href="{{ url('/') }}" class="theme-btn">Volver al Home <i class="far fa-home"></i></a>
-            </div>
-        </div>
-    </div>
+<section class="page-header page-header-modern bg-color-quaternary page-header-md custom-page-header">
+	<div class="container">
+		<div class="row mt-3">
+			<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+				<h1>- Error 504</h1>
+			</div>
+			<div class="col-md-4 order-1 order-md-2 align-self-center">
+				<ul class="breadcrumb d-block text-md-end breadcrumb-light">
+					<li><a href="{{ url('/') }}">Home</a></li>
+					<li class="active">Error 504</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+<div class="container">
+	<div class="row pt-1 pb-5 mb-3">
+		<div class="col">
+			<div class="row mb-3">
+				<div class="col">
+					<div class="feature-box custom-feature-box custom-feature-box-active feature-box-style-2">
+						<div class="feature-box-info ms-3">
+                            <center>
+                                <h2>Oops...</h2>
+                                <p>Lo sentimos, pero algo salió mal.</p>
+                                <a href="{{ url('/') }}" class="btn btn-primary">Volver al Home <i class="icon-home icons"></i></a>
+                            </center>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
